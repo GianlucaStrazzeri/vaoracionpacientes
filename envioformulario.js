43eb8edb217701756deb76c88c3cd4e13ab827df
GianlucaStrazzeri <githubandwebprojects@gmail.com>
2024-01-03 19:23:52 +0100

    Añadido las funciones de mobile first

---
+++ envioformulario.js
@@ -0,0 +1,9 @@
+
+
+function enviarFormulario() {
+    let nombre = document.getElementById("nombre").value;
+    let movil = document.getElementById("movil").value;
+    let fecha = document.getElementById("fecha").value;
+
+    console.log(nombre+movil+fecha);
+}
